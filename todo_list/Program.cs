858f3cc2ecc7dad011d96c40c371f4b130f026d3
aina-lang <merciaaina@gmail.com>
2026-02-26 23:18:43 +0300

    Fix task ID assignment and adjust initial task list indices

--- todo_list/Program.cs
+++ todo_list/Program.cs
@@ -6,9 +6,9 @@ var app = builder.Build();
 
 
 List<Task> tasks = [
-    new Task(1, "Task 1", "Description of Task 1", false, DateTime.Now, DateTime.Now),
-    new Task(2, "Task 2", "Description of Task 2", true, DateTime.Now, DateTime.Now),
-    new Task(3, "Task 3", "Description of Task 3", false, DateTime.Now, DateTime.Now)
+    new Task(0, "Task 1", "Description of Task 1", false, DateTime.Now, DateTime.Now),
+    new Task(1, "Task 2", "Description of Task 2", true, DateTime.Now, DateTime.Now),
+    new Task(2, "Task 3", "Description of Task 3", false, DateTime.Now, DateTime.Now)
 ];
 
 
@@ -23,13 +23,19 @@ app.MapGet("/tasks/{id}", (int id )=>tasks.FirstOrDefault(t=>t.ID == id));
 app.MapPost("/tasks",(CreateTaskDto createTaskDto) =>
 {
     var newTask = new Task(
-        tasks.Count + 1,
+        tasks.Count-1,
         createTaskDto.Title,
         createTaskDto.Description,
         false,
         DateTime.Now,
         DateTime.Now
     );
+
+    bool exist = tasks.Any(t => t.Title.Equals(createTaskDto.Title, StringComparison.OrdinalIgnoreCase));
+   if(exist)
+   {
+        return Results.BadRequest("Task already exists");
+   }
     tasks.Add(newTask);
     return Results.Created($"/tasks/{newTask.ID}", newTask);
 });
